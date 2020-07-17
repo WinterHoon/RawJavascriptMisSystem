6fae6947d390c825d178be04d63fd0d834edc166
Winter <winterhoon@WinterHoondeMacBook-Air.local>
2020-07-18 00:10:55 +0800

    实现了多选的表格渲染（合并单元格）

--- table.js
+++ table.js
@@ -5,6 +5,7 @@ import {allRegions, allProducts} from './checkbox';
 
 /*----------------表格展示相关操作----------------*/
 
+//获取选择的数据 return data
 function getData() {
     var regions = [];
     var products = [];
@@ -36,8 +37,15 @@ function getData() {
 
 
     }
-    newData = [...set1].filter(item => set2.has(item));
-    return newData;
+    // newData = [...set1].filter(item => set2.has(item));
+    //console.log(newData);
+    return {
+        newData: [...set1].filter(item => set2.has(item)),
+        productsNum: products.length,
+        regionsNum: regions.length
+    };
+    // return newData;
+    
 }
 
 //显示表格的方法
@@ -49,35 +57,57 @@ function displayTable() {
             child.remove();
         }
     } 
-    var data = getData();
+    let threeData = getData();
+    console.log(threeData);
+    var data = threeData.newData;
+    
     var table = document.createElement('table');
     //制作表头
     var thRow = document.createElement('tr');
-    var months = ['商品','地区','一月','二月','三月','四月','五月','六月','七月','八月','九月','十月','十一月','十二月'];
-    for(let i=0;i<months.length;i++) {
+    var tableHeads = ['商品','地区','一月','二月','三月','四月','五月','六月','七月','八月','九月','十月','十一月','十二月'];
+    for(let i=0;i<tableHeads.length;i++) {
         let th = document.createElement('th');
-        th.textContent = months[i];
+        th.textContent = tableHeads[i];
         thRow.appendChild(th);
     }
     table.appendChild(thRow);
 
-    //复制数据
+    //复制数据做表格渲染
     for (let i=0;i<data.length;i++) {
-        let tr = document.createElement('tr');
-        let td1 = document.createElement('td');
-        let td2 = document.createElement('td');
-        td1.textContent = data[i].product;
-        td2.textContent = data[i].region;
-        tr.appendChild(td1);
-        tr.appendChild(td2);
-        for(let j=0;j<data[i]['sale'].length;j++){
-            let td = document.createElement('td');
-            td.textContent = data[i]['sale'][j];
-            tr.appendChild(td);
+            let tr = document.createElement('tr');
+            let td1 = document.createElement('td');
+            let td2 = document.createElement('td');
 
-        
-        }
-        table.appendChild(tr);
+            //只有商品选多个地区选一个的时候需要把地区放在第一列
+            if (threeData.productsNum > 1 && threeData.regionsNum === 1) {
+                td1.textContent = data[i].region;
+                td2.textContent = data[i].product;
+                //虽然每行都生成了一个“地区”单元格，但只有指定行添加进了表格
+                if (i % threeData.productsNum === 0) {
+                    td1.rowSpan = threeData.productsNum;
+                    console.log(td1);
+                    tr.appendChild(td1);    
+                }
+            //其余情况都是商品放在第一列
+            } else {
+                td1.textContent = data[i].product;
+                td2.textContent = data[i].region;
+                //虽然每行都生成了一个“商品”单元格，但只有指定行添加进了表格
+                if (i % threeData.regionsNum === 0) {
+                    td1.rowSpan = threeData.regionsNum;
+                    console.log(td1);
+                    tr.appendChild(td1);
+                }   
+            }
+
+            tr.appendChild(td2);
+            for(let j=0;j<data[i]['sale'].length;j++) {
+                let td = document.createElement('td');
+                td.textContent = data[i]['sale'][j];
+                tr.appendChild(td);
+            }
+            table.appendChild(tr);
+
     }
     
     tableWrapper.appendChild(table);
